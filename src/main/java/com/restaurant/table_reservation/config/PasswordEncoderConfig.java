cb55837a2f572a9bd08a7bcb06ed0a284b46ca0b
feysel-hussien <150449709+feysel-hussien@users.noreply.github.com>
2025-02-01 13:16:08 +0300

    password encoding added

---
+++ src/main/java/com/restaurant/table_reservation/config/PasswordEncoderConfig.java
@@ -0,0 +1,12 @@
+package com.restaurant.table_reservation.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class PasswordEncoderConfig {
+
+   
+}
